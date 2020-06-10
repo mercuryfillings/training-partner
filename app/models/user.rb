12589ec463b85608fc30d55f47ceff30e8c51cc7
Scott Delbango <scott.delbango@gmail.com>
2020-06-10 15:08:14 -0400

    routes added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  validates :email, uniqueness: true, presence: true
   has_many :techniques, dependent: :destroy
   has_many :training_sessions, dependent: :destroy
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+Technique.destroy_all
+TrainingSession.destroy_all
+
+@user = User.create!({ name: 'mercuryfillings', email: 'scott.delbango@gmail.com', password: '123456' })
+
+p "#{User.count} user(s) created"
+
+Technique.create!(name: 'armbar', user: @user, category: 'submission', position: 'guard', orientation: 'bottom', times_executed: 1)
+
+p "#{Technique.count} technique(s) created"
+
+TrainingSession.create!(rolls: 5, opponents: 3, subs: 10, taps: 3)
+
+p "#{TrainingSession.count} training session(s) created"
