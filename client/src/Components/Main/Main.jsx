93eca0cc0b37b4094e375965ae4c17fc4070cde7
Scott Delbango <scott.delbango@gmail.com>
2020-06-12 14:27:27 -0400

    Basic training and technique crud methods added to main, but likely need serious editing

--- client/src/Components/Main/Main.jsx
+++ client/src/Components/Main/Main.jsx
@@ -2,6 +2,13 @@ import React, { Component } from 'react'
 import { Route } from 'react-router-dom';
 import Login from '../Login/Login'
 import Signup from '../Signup/Signup';
+import ShowOneTech from '../ShowOneTech/ShowOneTech'
+import ShowAllTechs from '../ShowAllTechs/ShowAllTechs'
+import CreateTech from '../CreateTech/CreateTech'
+import UpdateTech from '../UpdateTech/UpdateTech'
+import ShowTraining from '../ShowTraining/ShowTraining'
+import UpdateTraining from '../UpdateTraining/UpdateTraining'
+import CreateTraining from '../CreateTraining/CreateTraining'
 import { getAllTechs, getOneTech, createTech } from '../../Services/techniques'
 import { getAllTrainings, getOneTraining, createTraining } from '../../Services/trainings'
 import Home from '../Home/Home'
@@ -28,6 +35,27 @@ export default class Main extends Component {
     this.setState({ techniques });
   }
 
+  postTech = async (techData) => {
+    const newTechnique = await createTech(techData);
+    this.setState(prevState => ({
+      techniques: [...prevState.techniques, newTechnique]
+    }))
+  }
+
+  putTech = async (id, techData) => {
+    const updatedTechnique = await updateTechnique(id, techData);
+    this.setState(prevState => ({
+      techniques: prevState.techniquess.map(technique => technique.id === id ? updatedTechnique : technique)
+    }))
+  }
+
+  destroyTechnique = async (id) => {
+    await deleteTechnique(id);
+    this.setState(prevState => ({
+      techniques: prevState.techniques.filter(technique => technique.id !== id)
+    }))
+  }
+
   // ============================
   // === Training Sessions ======
   // ============================
@@ -35,6 +63,27 @@ export default class Main extends Component {
   getTrainings = async () => {
     const trainings = await getAllTrainings();
     this.setState({ trainings });
+  }
+
+  postTraining = async (trainingData) => {
+    const newTraining = await createTraining(trainingData);
+    this.setState(prevState => ({
+      trainings: [...prevState.trainings, newTraining]
+    }))
+  }
+
+  putTraining = async (id, trainingData) => {
+    const updatedTraining = await updateTraining(id, trainingData);
+    this.setState(prevState => ({
+      trainings: prevState.trainings.map(training => training.id === id ? updatedTraining : training)
+    }))
+  }
+
+  destroyTraining = async (id) => {
+    await deleteTraining(id);
+    this.setState(prevState => ({
+      trainings: prevState.trainings.filter(training => training.id !== id)
+    }))
   }
 
   render() {
@@ -55,6 +104,13 @@ export default class Main extends Component {
         <Route path='/techniques' render={() => (
           <ShowAllTechs />
         )} />
+        <Route path='/technique/:id' render={(props) => {
+          const techId = props.match.params.id;
+          return <ShowOneTech
+            techId={techId}
+            currentUser={this.props.currentUser}
+          />
+        }} />
         <Route exact path='/trainings' render={(props) => (
           <ShowTraining
             {...props}
@@ -78,13 +134,7 @@ export default class Main extends Component {
             putTechnique={this.putTechnique}
           />
         }} />
-        <Route path='/technique/:id' render={(props) => {
-          const techId = props.match.params.id;
-          return <ShowOneTech
-            techId={techId}
-            currentUser={this.props.currentUser}
-          />
-        }} />
+
       </main>
     )
   }
