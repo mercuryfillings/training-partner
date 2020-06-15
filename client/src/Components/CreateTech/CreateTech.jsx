458f04609881b42a15cee92e75c90be140e963bb
Scott Delbango <scott.delbango@gmail.com>
2020-06-15 00:06:28 -0400

    need a new branch

--- client/src/Components/CreateTech/CreateTech.jsx
+++ client/src/Components/CreateTech/CreateTech.jsx
@@ -1,6 +1,13 @@
 import React, { Component } from 'react'
 
 export default class CreateTech extends Component {
+  state = {
+    name: "",
+    category: "",
+    position: "",
+    orientation: ""
+  }
+
   render() {
     return (
       <div>
