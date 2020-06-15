09e062ff63a7fe3d6720496d07277f3ce8ab474e
Scott Delbango <scott.delbango@gmail.com>
2020-06-15 00:27:17 -0400

    Not quite done, but skeleton's there. Needs logic.

--- client/src/Components/CreateTech/CreateTech.jsx
+++ client/src/Components/CreateTech/CreateTech.jsx
@@ -8,11 +8,57 @@ export default class CreateTech extends Component {
     orientation: ""
   }
 
+  handleChange = (e) => {
+    const { value } = e.target;
+    this.setState({
+      name: value
+    })
+  }
+
   render() {
+    const { name, category, position, orientation } = this.state
+    const { postTechnique, history } = this.props
     return (
-      <div>
-        
-      </div>
+      <form onSubmit={(e) => {
+        e.preventDefault();
+        postTechnique(this.state);
+        history.push('/techniques');
+        this.setState({
+          name: ""
+        })
+      }}>
+        <hr />
+        <h3>Add New Technique</h3>
+        <label htmlFor="name">Name:</label>
+        <input
+          id="id"
+          type="text"
+          value={name}
+          onChange={this.handleChange}
+        />
+        <label htmlFor="category">Category:</label>
+        <select>
+          <option value="Submission">Submission</option>
+          <option value="Sweep">Sweep</option>
+          <option value="Guard Pass">Guard Pass</option>
+          <option value="Escape">Escape</option>
+        </select>
+          <label htmlFor="position">Position:</label>
+          <select>
+            <option value="Guard">Guard</option>
+            <option value="Mount">Mount</option>
+            <option value="Turtle">Turtle</option>
+            <option value="Knee on Belly">Knee on Belly</option>
+            <option value="Side Control">Side Control</option>
+            <option value="Back">Back</option>
+        </select>
+          <label htmlFor="orientation">Orientation:</label>
+          <select>
+            <option value="Bottom">Bottom</option>
+            <option value="Top">Top</option>
+        </select>
+        <button>Submit</button>
+      </form>
     )
   }
 }
