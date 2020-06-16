46a3d94afbec93ad8965ff04456d14da64664a74
Scott Delbango <scott.delbango@gmail.com>
2020-06-15 22:21:06 -0400

    Add Tech desktop styled

--- client/src/Components/CreateTech/CreateTech.jsx
+++ client/src/Components/CreateTech/CreateTech.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import './CreateTech.scss'
 
 export default class CreateTech extends Component {
   state = {
@@ -21,7 +22,7 @@ export default class CreateTech extends Component {
     const { name } = this.state
     const { postTech, history } = this.props
     return (
-      <form onSubmit={(e) => {
+      <form className="tech-form" onSubmit={(e) => {
         e.preventDefault();
         postTech(this.state);
         history.push('/techniques');
@@ -29,40 +30,42 @@ export default class CreateTech extends Component {
           name: ""
         })
       }}>
-        <h3>Add New Technique</h3>
-        <label htmlFor="name">Name:</label>
-        <input
-          id="id"
-          type="text"
-          name="name"
-          value={name}
-          onChange={this.handleChange}
-        />
-        <label htmlFor="category">Category:</label>
-        <select name="category" defaultValue={'DEFAULT'} onChange={this.handleChange}>
-          <option value="DEFAULT" disabled>- Select -</option>
-          <option value="Submission">Submission</option>
-          <option value="Sweep">Sweep</option>
-          <option value="Guard Pass">Guard Pass</option>
-          <option value="Escape">Escape</option>
-        </select>
-        <label htmlFor="position">Position:</label>
-        <select name="position" defaultValue={'DEFAULT'} onChange={this.handleChange}>
-          <option value="DEFAULT" disabled>- Select -</option>
-          <option value="Guard">Guard</option>
-          <option value="Mount">Mount</option>
-          <option value="Turtle">Turtle</option>
-          <option value="Knee on Belly">Knee on Belly</option>
-          <option value="Side Control">Side Control</option>
-          <option value="Back">Back</option>
-        </select>
-        <label htmlFor="orientation">Orientation:</label>
-        <select name="orientation" defaultValue={'DEFAULT'} onChange={this.handleChange}>
-          <option value="DEFAULT" disabled>- Select -</option>
-          <option value="Bottom">Bottom</option>
-          <option value="Top">Top</option>
-        </select>
-        <button>Submit</button>
+        <h1 className="add">Add Technique</h1>
+        <div className="add-page-container">
+          <div className="add-items-container">
+          <input
+            id="id"
+            className="tech-input"
+            type="text"
+            name="name"
+            placeholder="Technique Name"
+            value={name}
+            onChange={this.handleChange}
+          />
+          <select className="tech-select" name="category" defaultValue={'DEFAULT'} onChange={this.handleChange}>
+            <option value="DEFAULT" disabled>Category</option>
+            <option value="Submission">Submission</option>
+            <option value="Sweep">Sweep</option>
+            <option value="Guard Pass">Guard Pass</option>
+            <option value="Escape">Escape</option>
+          </select>
+          <select className="tech-select" name="position" defaultValue={'DEFAULT'} onChange={this.handleChange}>
+            <option value="DEFAULT" disabled>Position</option>
+            <option value="Guard">Guard</option>
+            <option value="Mount">Mount</option>
+            <option value="Turtle">Turtle</option>
+            <option value="Knee on Belly">Knee on Belly</option>
+            <option value="Side Control">Side Control</option>
+            <option value="Back">Back</option>
+          </select>
+          <select className="tech-select" name="orientation" defaultValue={'DEFAULT'} onChange={this.handleChange}>
+            <option value="DEFAULT" disabled>Your Orientation</option>
+            <option value="Bottom">Bottom</option>
+            <option value="Top">Top</option>
+          </select>
+          </div>
+          <button className="save-tech">Save</button>
+        </div>
       </form>
     )
   }
