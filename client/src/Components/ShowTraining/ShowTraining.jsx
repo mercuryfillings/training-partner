9db67918b2a9a76f7617ff1046816a6bb15a38b2
Scott Delbango <scott.delbango@gmail.com>
2020-06-16 00:11:34 -0400

    Training history desktop styled

--- client/src/Components/ShowTraining/ShowTraining.jsx
+++ client/src/Components/ShowTraining/ShowTraining.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import './ShowTraining.scss'
 
 export default function ShowTraining(props) {
   const { trainings, currentUser, history } = props;
 
   return (
-    <>
-      <select>
+    <div className="training-body">
+      <h1 className="history-title">Training History</h1>
+      <div className="training-container">
+        <button className="training-button">Add Training</button>
+        <div className="training-container-inner">
+      <select className="date-select">
         <option value="Past 7 Days">Past 7 Days</option>
         <option value="Past 30 Days">Past 30 Days</option>
         <option value="Past 365 Days">Past 365 Days</option>
@@ -14,14 +19,15 @@ export default function ShowTraining(props) {
       {
         trainings.map(training => (
           <React.Fragment key={training.created_at}>
-            <h2>{training.created_at}</h2>
-            <p>Rolls: {training.rolls}</p>
-            <p>Opponents: {training.opponents}</p>
-            <p>Subs: {training.subs}</p>
-            <p>Taps: {training.taps}</p>
-            <br />
+            <h1 className="date">{training.created_at}</h1>
+            <p className="info">Rolls: {training.rolls}</p>
+            <p className="info">Opponents: {training.opponents}</p>
+            <p className="info">Subs: {training.subs}</p>
+            <p className="info">Taps: {training.taps}</p>
           </React.Fragment>
         ))}
-    </>
+          </div>
+        </div>
+    </div>
   )
 }
